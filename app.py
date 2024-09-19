9dfcd30b2965f66ee2ddac7c68a16bf2ca8bf5eb
root <root@Ubuntu4.myguest.virtualbox.org>
2024-09-18 19:06:29 -0500

    iEnhance App

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from datetime import datetime
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-	return 'Hello MSOE'
+	today = datetime.today().strftime('%Y-%m-%d')
+	return f'Hello MSOE friends and faculty. Today, {today} is going to be a good day!'
 
